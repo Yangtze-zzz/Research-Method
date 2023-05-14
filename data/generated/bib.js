795b56c2c44425ff7138c6f443a1954dc0f71938
Se7en <48167404+Yangtze-zzz@users.noreply.github.com>
2023-05-14 13:10:22 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,118 @@
+﻿define({ entries : {
+    "Arthur2021": {
+        "author": "Arthur Emanuel de Oliveira Carosia and Guilherme Palermo Coelho and Ana Estela Antunes da Silva",
+        "doi": "10.1016/j.eswa.2021.115470",
+        "issn": "09574174",
+        "journal": "Expert Systems with Applications",
+        "keywords": "type:Case Study, data source:social media and news, sentiment analysis, artificial neural networks, MLP, LSTM, CNN, stock market",
+        "month": "12",
+        "pages": "115470",
+        "publisher": "Elsevier BV",
+        "title": "Investment strategies applied to the Brazilian stock market: A methodology based on Sentiment Analysis with deep learning",
+        "type": "article",
+        "volume": "184",
+        "year": "2021"
+    },
+    "Cui2023": {
+        "author": "Jingfeng Cui and Zhaoxia Wang and Seng Beng Ho and Erik Cambria",
+        "doi": "10.1007/s10462-022-10386-z",
+        "issn": "15737462",
+        "journal": "Artificial Intelligence Review",
+        "keywords": "type:Survey Paper, data source:social media and news,",
+        "title": "Survey on sentiment analysis: evolution of research methods and topics",
+        "type": "article",
+        "year": "2023"
+    },
+    "Jin2020": {
+        "author": "Zhigang Jin and Yang Yang and Yuhong Liu",
+        "doi": "10.1007/s00521-019-04504-2",
+        "issn": "14333058",
+        "issue": "13",
+        "journal": "Neural Computing and Applications",
+        "keywords": "type:Technical Analysis, data source:social media,",
+        "title": "Stock closing price prediction based on sentiment analysis and LSTM",
+        "type": "article",
+        "volume": "32",
+        "year": "2020"
+    },
+    "Kearney2014": {
+        "author": "Colm Kearney and Sha Liu",
+        "doi": "10.1016/j.irfa.2014.02.006",
+        "issn": "10575219",
+        "journal": "International Review of Financial Analysis",
+        "keywords": "type:Survey Paper, data source:social media, behavioral finance, textual sentiment, Internet messages, news, market efficiency",
+        "title": "Textual sentiment in finance: A survey of methods and models",
+        "type": "generic",
+        "volume": "33",
+        "year": "2014"
+    },
+    "Li2020": {
+        "author": "Xiaodong Li and Pangjing Wu and Wenpeng Wang",
+        "doi": "10.1016/j.ipm.2020.102212",
+        "issn": "03064573",
+        "issue": "5",
+        "journal": "Information Processing and Management",
+        "keywords": "type:Case Study, data source:news, stock prediction, news sentiment, analysis, deep learning",
+        "title": "Incorporating stock prices and news sentiments for stock market prediction: A case of Hong Kong",
+        "type": "article",
+        "volume": "57",
+        "year": "2020"
+    },
+    "Li2021": {
+        "author": "Menggang Li and Wenrui Li and Fang Wang and Xiaojun Jia and Guangwei Rui",
+        "doi": "10.1007/s00521-020-05411-7",
+        "issn": "14333058",
+        "issue": "10",
+        "journal": "Neural Computing and Applications",
+        "keywords": "type:Technical Analysis, data source:social media,",
+        "title": "Applying BERT to analyze investor sentiment in stock market",
+        "type": "article",
+        "volume": "33",
+        "year": "2021"
+    },
+    "Malandri2018": {
+        "author": "Lorenzo Malandri and Frank Z. Xing and Carlotta Orsenigo and Carlo Vercellis and Erik Cambria",
+        "doi": "10.1007/s12559-018-9609-2",
+        "issn": "18669964",
+        "issue": "6",
+        "journal": "Cognitive Computation",
+        "keywords": "type:Technical Analysis, data source:social media,",
+        "title": "Public Mood\u2013Driven Asset Allocation: the Importance of Financial Sentiment in Portfolio Management",
+        "type": "article",
+        "volume": "10",
+        "year": "2018"
+    },
+    "Muthivhi2022": {
+        "author": "Mufhumudzi Muthivhi and Terence L. Van Zyl",
+        "doi": "10.23919/FUSION49751.2022.9841261",
+        "journal": "2022 25th International Conference on Information Fusion, FUSION 2022",
+        "keywords": "type:Algorithm, data source:social media and news",
+        "title": "Fusion of Sentiment and Asset Price Predictions for Portfolio Optimization",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "Ren2019": {
+        "author": "Rui Ren and Desheng Dash Wu and Desheng Dash Wu",
+        "doi": "10.1109/JSYST.2018.2794462",
+        "issn": "19379234",
+        "issue": "1",
+        "journal": "IEEE Systems Journal",
+        "keywords": "type:Algorithm, data source:social media, Day-of-week effect, decision making, sentiment analysis, stock markets, text mining",
+        "title": "Forecasting stock market movement direction using sentiment analysis and support vector machine",
+        "type": "article",
+        "volume": "13",
+        "year": "2019"
+    },
+    "Xing2018": {
+        "author": "Frank Z. Xing and Erik Cambria and Roy E. Welsch",
+        "doi": "10.1109/MCI.2018.2866727",
+        "issn": "15566048",
+        "issue": "4",
+        "journal": "IEEE Computational Intelligence Magazine",
+        "keywords": "type:Algorithm, data source:social media, stock markets, covariance matrices, sentiment analysis, bayes methods, resource management, mathematical model, computational modeling, time series analysis, analytical models",
+        "title": "Intelligent asset allocation via market sentiment views",
+        "type": "article",
+        "volume": "13",
+        "year": "2018"
+    }
+}});
